08f656fe1ea71dc0d315f18ea7e83cbe3eec0b7a
ThomasGauvinLagier <thomas.gauvinlag@gmail.com>
2025-12-16 14:31:52 +0100

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php
+	echo "<div class='menu' style='background:pink; width:150px; cursor:pointer;'>";
+        echo "<p><b>La boutique à zaza</b></p>";
+        echo "<div>";
+            echo "<a href='https://fr.pinterest.com/'>DVD</a><br>";
+            echo "<a href='https://fr.pinterest.com/'>Livre à ZAZA</a>";
+        echo "</div>";
+    echo "</div>";
+?>
